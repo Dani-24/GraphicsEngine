7b86174cd1d4db886d2f107a3bb2668e3e0b7ccc
Dani <79161140+Dani-24@users.noreply.github.com>
2024-03-14 08:20:10 +0100

    Buffer Functions finished

--- Engine/Code/BufferFunc.h
+++ Engine/Code/BufferFunc.h
@@ -16,61 +16,29 @@ namespace BufferManager {
 #define PushMat3(buffer, value) PushAlignedData(buffer, value_ptr(value), sizeof(value), sizeof(vec4))
 #define PushMat4(buffer, value) PushAlignedData(buffer, value_ptr(value), sizeof(value), sizeof(vec4))
 
-	bool IsPowerOf2(u32 value)
-	{
-		return value && !(value & (value - 1));
-	}
+	struct Buffer {
+		u32 size;
+		GLenum type;
+		GLuint handle;
+		u8* data;
+		int head;
+	};
 
-	u32 Align(u32 value, u32 alignment)
-	{
-		return (value + alignment - 1) & ~(alignment - 1);
-	}
+	bool IsPowerOf2(u32 value);
 
-	Buffer CreateBuffer(u32 size, GLenum type, GLenum usage)
-	{
-		Buffer buffer = {};
-		buffer.size = size;
-		buffer.type = type;
+	u32 Align(u32 value, u32 alignment);
 
-		glGenBuffers(1, &buffer.handle);
-		glBindBuffer(type, buffer.handle);
-		glBufferData(type, buffer.size, NULL, usage);
-		glBindBuffer(type, 0);
+	Buffer CreateBuffer(u32 size, GLenum type, GLenum usage);
 
-		return buffer;
-	}
+	void BindBuffer(const Buffer& buffer);
 
-	void BindBuffer(const Buffer& buffer)
-	{
-		glBindBuffer(buffer.type, buffer.handle);
-	}
+	void MapBuffer(Buffer& buffer, GLenum access);
 
-	void MapBuffer(Buffer& buffer, GLenum access)
-	{
-		glBindBuffer(buffer.type, buffer.handle);
-		buffer.data = (u8*)glMapBuffer(buffer.type, access);
-		buffer.head = 0;
-	}
+	void UnmapBuffer(Buffer& buffer);
 
-	void UnmapBuffer(Buffer& buffer)
-	{
-		glUnmapBuffer(buffer.type);
-		glBindBuffer(buffer.type, 0);
-	}
+	void AlignHead(Buffer& buffer, u32 alignment);
 
-	void AlignHead(Buffer& buffer, u32 alignment)
-	{
-		ASSERT(IsPowerOf2(alignment), "The alignment must be a power of 2");
-		buffer.head = Align(buffer.head, alignment);
-	}
-
-	void PushAlignedData(Buffer& buffer, const void* data, u32 size, u32 alignment)
-	{
-		ASSERT(buffer.data != NULL, "The buffer must be mapped first");
-		AlignHead(buffer, alignment);
-		memcpy((u8*)buffer.data + buffer.head, data, size);
-		buffer.head += size;
-	}
+	void PushAlignedData(Buffer& buffer, const void* data, u32 size, u32 alignment);
 }
 
 #endif // !BUFFER_MANAGER_FUNC
